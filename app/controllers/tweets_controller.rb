e68bf50d02306de237d536dbb4b1e9410f5c94fd
github username <github email address>
2019-06-04 17:59:37 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,17 +1,32 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-  if logged_in?
-    @tweets = Tweet.all
-    erb :'/tweets/tweets'
-  else
-    redirect to '/login'
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'/tweets/tweets'
+    else
+      redirect to '/login'
+    end
   end
-end
 
-  # get '/tweets/:id' do
-  #   @user = User.find(params[:id])
-  #   erb :'/tweets/show'
-  # end
+  post '/tweets' do
+    user = current_user
+    user.tweets << Tweet.create(params[:tweet])
+
+    redirect "/tweets/#{user.tweets.last.id}"
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+      @tweet = Tweet.find(params[:id])
+    erb :'/tweets/show_tweet'
+  end
 
 end
