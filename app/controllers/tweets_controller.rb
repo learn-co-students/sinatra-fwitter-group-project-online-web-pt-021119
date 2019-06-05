5e667396efbc246c4b736cf71c97b4f1521c05b4
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-06-05 15:02:36 -0500

    Add file structure and basic routes and models

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,23 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    erb :'tweets/tweets'
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/create_tweet'
+  end
+
+  post '/tweets' do
+
+  end
+
+  get '/tweets/:id' do
+    erb :'tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+    erb :'tweets/edit_tweet'
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  get '/tweets/:id/edit' do
+    erb :'tweets/edit_tweet'
+  end
+
+  get '/logout' do
+    erb :index
+  end
 
 end
