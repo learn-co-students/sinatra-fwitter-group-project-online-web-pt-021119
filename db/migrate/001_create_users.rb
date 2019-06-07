573fe8e240fe336f8e40199a1cd299b73e704d97
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-06-07 10:13:14 -0500

    add migrations and associations in models

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,2 +1,12 @@
 class CreateUsers < ActiveRecord::Base
+
+  def change
+    create_table :users do |x|
+      x.string :username
+      x.text :email
+      x.string :password_digest
+
+      x.timestamps
+    end
+  end
 end
--- db/migrate/002_create_tweets.rb
+++ db/migrate/002_create_tweets.rb
@@ -1,3 +1,8 @@
 class CreateTweets < ActiveRecord::Base
-
+  def change
+    create_table :tweets do |x|
+      x.text :content
+      x.integer :user_id
+    end
+  end
 end
