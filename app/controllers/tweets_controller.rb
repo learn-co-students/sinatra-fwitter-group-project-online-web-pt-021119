40e65ace01a41cf6f72d6534292c5a2854e2cd31
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-06-07 11:42:51 -0500

    fix tweet routes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,35 +1,36 @@
 class TweetsController < ApplicationController
 
-  get '/tweets' do
+  get '/tweets' do #works
     @tweets = Tweet.all
     erb :'tweets/tweets'
   end
 
-  get '/tweets/new' do
-    erb :'tweets/create_tweet'
+  get '/tweets/new' do #works
+    erb :'tweets/new'
   end
 
-  post '/tweets' do
-    @tweet = params[:content]
+  post '/tweets' do #works
+    @tweet = Tweet.create(content: params[:content])
     redirect to "/tweets/#{@tweet.id}"
   end
 
-  get '/tweets/:id' do
-    @tweet = Tweet.find_by_id(params[:id])
+  get '/tweets/:id' do #works
+    @tweet = Tweet.find(params[:id])
     erb :'tweets/show_tweet'
   end
 
-  get '/tweets/:id/edit' do
+  get '/tweets/:id/edit' do #works
     @tweet = Tweet.find_by_id(params[:id])
     erb :'tweets/edit_tweet'
   end
 
-  patch '/tweets/:id' do
+  patch '/tweets/:id' do #works
     @tweet = Tweet.find_by_id(params[:id])
+    @tweet.update(content: params[:content])
     redirect to "/tweets"
   end
 
-  delete '/tweets/:id/delete' do
+  delete '/tweets/:id' do #works
     @tweet = Tweet.find_by_id(params[:id])
     @tweet.delete
     redirect to "/tweets"
