7a92456a3aa6fadfabea5a473243d62bb2551038
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-12 20:24:47 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,27 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  get '/tweets/:id' do
+    erb: "/tweets/#{tweet.id}"
+  end
+
+  get '/tweets/:id/edit' do
+    erb :'/tweets/edit'
+  end
+
+  post '/tweets' do
+    # some code collecting data from create tweet form
+
+    redirect to "/tweets/#{tweet.id}"
+  end
+
+  patch '/tweet/:id' do
+    # some code collecting data from edit tweet form
+
+    redirect to "/tweets/#{tweet.id}"
+  end
 
 end
