d685b96bd09f07484a95e057f80adfb30e4c3e79
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-12 22:13:26 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.create(username: params[:username], password: params[:password])
+  end
 
 end
