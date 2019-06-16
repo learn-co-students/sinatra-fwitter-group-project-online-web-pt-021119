e9d7dd009cafcd7a3bc6115005c2f187ff7d2f1b
Carlton Dwayne <carltondgrandison@gmail.com>
2019-06-16 20:55:15 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
     erb :'/users/signup'
   end
 
+  post "/signup" do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      @user = User.create(username: params[:username], password: params[:password])
+      redirect '/tweets'
+    end
+  end
+
+
 end
