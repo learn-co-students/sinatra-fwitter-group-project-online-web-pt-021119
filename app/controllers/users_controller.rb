6e6d05ebb802d9f8e3206b34890858572d5567ed
Allen <allenfp@gmail.com>
2019-06-25 06:51:53 -0700

    checkpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
 
     get '/signup' do 
-        erb :signup
+        erb :'users/signup'
     end
 
-    post '/signup' do
-        if params[:username] == "" || params[:email] == "" || params[:password] == ""
-            redirect '/signup'
-        end
-        # binding.pry
-        @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-        session[:user_id] = @user.id
-        redirect :"/tweets"
-    end
+    # post '/signup' do
+    #     if params[:username] == "" || params[:email] == "" || params[:password] == ""
+    #         redirect '/signup'
+    #     end
+    #     # binding.pry
+    #     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    #     session[:user_id] = @user.id
+    #     redirect :"/tweets"
+    # end
 
 end
