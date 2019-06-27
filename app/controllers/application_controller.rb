33cd38bb9dba97ddce9fd6a2c994bd2a15e91123
Allen <allenfp@gmail.com>
2019-06-27 09:39:10 -0700

    CHeckpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
   end
@@ -11,7 +12,15 @@ class ApplicationController < Sinatra::Base
     erb :index 
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
 
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,35 @@
 class UsersController < ApplicationController
 
     get '/signup' do 
-        erb :'users/signup'
+        if logged_in?
+            redirect to '/tweets'
+        else
+            erb :'users/signup'
+        end
+
     end
 
-    # post '/signup' do
-    #     if params[:username] == "" || params[:email] == "" || params[:password] == ""
-    #         redirect '/signup'
-    #     end
-    #     # binding.pry
-    #     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-    #     session[:user_id] = @user.id
-    #     redirect :"/tweets"
-    # end
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect '/signup'
+        end
+        @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+        session[:user_id] = @user.id
+        redirect '/tweets'
+    end
+
+    get '/login' do
+        erb :'users/login'
+    end
+
+    post '/login' do
+        # binding.pry
+        @user = User.find_by(:username => params[:username])
+        if @user != nil && @user.password == params[:password]
+            session[:user_id] = @user.id
+            redirect '/tweets'
+        end
+        redirect '/login'
+    end
 
 end
